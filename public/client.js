b41a974a5f1b3a4b7326613912245bc015a1d9ea
Glitch (diary) <none>
2018-09-03 16:17:08 +0000

    👝🕰 Checkpoint ./views/index.html:1020861/318 ./public/client.js:1020861/940

--- public/client.js
+++ public/client.js
@@ -1,5 +1,19 @@
+/* globals axios */
+
 var init = function() {
+  //create top of table
+  let html = "<table><tr><th>id</th><th>Date</th><th>Country</th><th>Place</th><th>Notes</th></tr>"
   
+  //get data from server
+  axios.get("/data").then((response) => {
+    let data = response.data
+    for (let i in data) {
+      html += "<tr><td>
+    }
+    
+    //update page
+    document.getElementById('table').innerHTML = html + "</table>"
+  })
 }
 
 window.onload = init
