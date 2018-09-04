f429f5cd93dab05b3e952d14598630b8767eb050
Glitch (diary) <none>
2018-09-04 13:07:31 +0000

    🕵🚄 Checkpoint ./views/index.html:1020861/368 ./public/client.js:1020861/884

--- public/client.js
+++ public/client.js
@@ -1,11 +1,15 @@
 /* globals axios, dateFormat */
 
-//gets all data
 var init = function() {
+  getData("/data")
+}
+
+//gets all data
+var getData = function(path) {
   let html;
   
   //get data from server
-  axios.get("/data").then((response) => {
+  axios.get(path).then((response) => {
     let data = response.data
     if (data == "") {
       html = "No records"
@@ -29,7 +33,7 @@ var init = function() {
 //deletes a record
 var remove = function(id) {
   axios.get("/delete?id=" + id).then((response) => {
-    init()
+    getData("/data")
   })
 }
 
@@ -38,28 +42,7 @@ var viewDates = function() {
   //get dates
   let from = new Date(document.getElementById('fromDate').value).getTime()
   let to = new Date(document.getElementById('toDate').value).getTime()
-  let html;
-  
-  //get data from server
-  axios.get("/dates?from=" + from + "&to=" + to).then((response) => {
-    let data = response.data
-    if (data == "") {
-      html = "No records"
-    } else {
-      //create top of table
-      html = "<table><tr><th>Date</th><th>Country</th><th>Place</th><th>Notes</th><th>Delete</th></tr>"
-  
-      for (let i in data) {
-        let x = data[i]
-        let y = dateFormat(x.date, "dS mmm. yyyy")
-        let z = `<button onclick="remove(${x.id})">X</button>`
-        html += `<tr><td>${y}</td><td>${x.country}</td><td>${x.place}</td><td>${x.notes}</td><td>${z}</td></tr>`
-      }
-    }
-    
-    //update page
-    document.getElementById('table').innerHTML = html + "</table>"
-  })
+  getData("/dates?from=" + from + "&to=" + to)
 }
 
 window.onload = init
