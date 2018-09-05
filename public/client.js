fdb75f8ae22b2166549419e7ab3f1ee9769d8e60
Glitch (diary) <none>
2018-09-05 11:56:14 +0000

    initial commit

---
+++ public/client.js
@@ -0,0 +1,62 @@
+/* globals axios, dateFormat */
+var view = "/data"
+
+var init = function() {
+  getData("/data")
+}
+
+//gets all data
+var getData = function(path) {
+  view = path
+  let html;
+  
+  //get data from server
+  axios.get(path).then((response) => {
+    let data = response.data
+    if (data == "") {
+      html = "No results"
+    } else {
+      //create top of table
+      html = "<table><tr><th>Date</th><th>Country</th><th>Place</th><th>Notes</th><th>Delete</th></tr>"
+  
+      for (let i in data) {
+        let x = data[i]
+        let y = dateFormat(x.date, "dS mmm. yyyy")
+        let z = `<button onclick="remove(${x.id})">X</button>`
+        html += `<tr><td>${y}</td><td>${x.country}</td><td>${x.place}</td><td>${x.notes}</td><td>${z}</td></tr>`
+      }
+    }
+    
+    //update page
+    document.getElementById('table').innerHTML = html + "</table>"
+  })
+}
+
+//deletes a record
+var remove = function(id) {
+  axios.get("/delete?id=" + id).then((response) => {
+    getData(view)
+  })
+}
+
+//show records between dates
+var viewDates = function() {
+  //get dates
+  let from = new Date(document.getElementById('fromDate').value).getTime()
+  let to = new Date(document.getElementById('toDate').value).getTime()
+  getData("/dates?from=" + from + "&to=" + to)
+}
+
+//search for a country
+var searchCountry = function() {
+  let country = document.getElementById('country').value
+  getData("/search?country=" + country)
+}
+
+//search for a place
+var searchPlace = function() {
+  let place = document.getElementById('place').value
+  getData("/search?place=" + place)
+}
+
+window.onload = init
---
+++ server.js
@@ -0,0 +1,103 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+var sqlite = require('sqlite3')
+require('./db.js')()
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}))
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/new', function(request, response) {
+  response.sendFile(__dirname + '/views/new.html');
+})
+
+app.post('/new', function(request, response) {
+  //get data
+  let date = new Date(request.body.date).getTime()
+  let country = request.body.country
+  let place = request.body.place
+  let notes = request.body.notes
+  
+  //make sure fields aren't blank
+  if (!(date == "" || country == "" || place == "")) {
+    //insert into db
+    let sql = `INSERT INTO places (date, country, place, notes) VALUES ("${date}", "${country}", "${place}", "${notes}");`
+    let db = new sqlite.Database('./.data/diary.db')
+    db.run(sql, [], (err) => {if (err) throw err});
+    db.close()
+  }
+  
+  response.redirect('/')
+})
+
+app.get('/data', function(request, response) {
+  //get data from db
+  let sql = "SELECT rowid AS id, date, country, place, notes FROM places ORDER BY date DESC, country ASC, place ASC"
+  let db = new sqlite.Database('./.data/diary.db')
+  db.all(sql, [], (err, data) => {
+    if (err) throw err;
+    response.send(data)
+  })
+db.close()
+})
+
+app.get('/dates', function(request, response) {
+  //get dates
+  let from = request.query.from
+  let to = request.query.to
+  
+  //get data from db
+  let sql = "SELECT rowid AS id, date, country, place, notes FROM places WHERE date>" + from + " AND date<" + to + " ORDER BY date DESC, country ASC, place ASC"
+  let db = new sqlite.Database('./.data/diary.db')
+  db.all(sql, [], (err, data) => {
+    if (err) throw err;
+    response.send(data)
+  })
+db.close()
+})
+
+app.get('/search', function(request, response) {
+  //get search term
+  let country = request.query.country
+  let place = request.query.place
+  let search
+  if (country) {
+    search = 'WHERE country LIKE "' + country + '"'
+  } else {
+    search = 'WHERE place LIKE "' + place + '"'
+  }
+  
+  //get data from db
+  let sql = "SELECT rowid AS id, date, country, place, notes FROM places " + search + " ORDER BY date DESC, country ASC, place ASC"
+  let db = new sqlite.Database('./.data/diary.db')
+  db.all(sql, [], (err, data) => {
+    if (err) throw err;
+    response.send(data)
+  })
+db.close()
+})
+
+app.get('/delete', function(request, response) {
+  let sql = `DELETE FROM places WHERE rowid=${request.query.id}`
+  let db = new sqlite.Database('./.data/diary.db')
+  db.run(sql, [], (err) => {if (err) throw err});
+  
+  response.redirect('/')
+})
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
