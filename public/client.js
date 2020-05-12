7e63350a296194767f17d30b5f62d39d6e089333
Glitch (diary) <none>
2020-05-12 20:42:52 +0000

    ✍️🌮 Checkpoint ./views/diary.html:1020861/11778 ./public/client.js:1020861/9133

--- public/client.js
+++ public/client.js
@@ -86,6 +86,30 @@ var normalSearch = function() {
   resetSearchForm()
 }
 
+// Date search
+var dateSearch = function() {
+  let field = document.getElementById("searchFieldDate")
+  
+  if (field.value == "year") {
+    let year = document.getElementById("searchYear").value;
+    // Get the start and end of the year
+    // Plus / minus one is hack because the server uses < and > rather than <= and >=
+    let start = new Date(year, 0, 1).getTime() - 1
+    let end = new Date(year, 11, 31).getTime() + 1
+    getData(`/dates?from=${start}&to=${end}`)
+  } else {
+    let year = document.getElementById("searchYear").value;
+    let month = document.getElementById("searchMonth").value;
+    // Get the start and end of the month
+    // JS uses 0 based month indexes, so minus one
+    let start = new Date(year, month - 1, 1).getTime() - 1;
+    let end = new Date(year, month, 1).getTime();
+    getData(`/dates?from=${start}&to=${end}`)
+  }
+  
+  resetSearchForm()
+}
+
 // Search field changed
 var searchFieldChanged = function(el) {
   //change to the correct search input
@@ -99,11 +123,51 @@ var searchFieldChanged = function(el) {
   }
 }
 
+// Search field changed for date
+var searchFieldChangedDate = function(el) {
+  //change to the correct search input
+  if (el.value == "year") {
+    // Remove month field
+    document.getElementById("monthWrapper").innerHTML = "";
+  } else {
+    // Create the month element
+    let month = document.getElementById("monthWrapper")
+    month.innerHTML = `<li class="item-content item-input">
+                       <div class="item-media">Month</div>
+                        <div class="item-inner">
+                          <div class="item-input-wrap">
+                            <select id="searchMonth">
+                                <option value="1">January</option>
+                                <option value="2">February</option>
+                                <option value="3">March</option>
+                                <option value="4">April</option>
+                                <option value="5">May</option>
+                                <option value="6">June</option>
+                                <option value="7">July</option>
+                                <option value="8">August</option>
+                                <option value="9">September</option>
+                                <option value="10">October</option>
+                                <option value="11">November</option>
+                                <option value="12">December</option>
+                            </select>
+                          </div>
+                        </div>
+                        </li>`
+  }
+}
+
 var resetSearchForm = function() {
+  // Normal search
   let field = document.getElementById("searchField")
   field.value = "other"
   searchFieldChanged(field)
   document.getElementById("searchText").value = ""
+  
+  // Date search
+  field = document.getElementById("searchFieldDate")
+  field.value = "year"
+  searchFieldChangedDate(field)
+  document.getElementById("searchYear").value = ""
 }
 
 window.onload = init
