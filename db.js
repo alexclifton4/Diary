0472464f45794009657e7392744e18d084b077c5
Alex Clifton <20780073+alexclifton4@users.noreply.github.com>
2021-01-14 16:05:33 +0000

    Remove DB credentials

--- db.js
+++ db.js
@@ -2,8 +2,7 @@ const { Client } = require('pg');
 
 exports.connect = function(){
   let client = new Client({
-  //connectionString: process.env.DATABASE_URL,
-  connectionString: "postgres://wvbdtrwkaodouy:67a2327ace4c1a33285fc60864df3d3590d484def8a39446b1f3a67a24eafa29@ec2-54-75-244-161.eu-west-1.compute.amazonaws.com:5432/d7sh93rseopa3k",
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
